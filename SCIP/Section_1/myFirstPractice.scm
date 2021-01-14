0688ba6fc49402fee6e3e366db2e00d00406d4fb
MattP34 <mattthewpropp@westmister.net>
2021-01-14 14:00:14 -0500

    in class scam lesson 1

---
+++ SCIP/Section_1/myFirstPractice.scm
@@ -0,0 +1,9 @@
+(define (main)
+    (println "Author: Matthew Propp matthewpropp@westminster.net")
+)
+
+; Problem 1
+; Phone number: 1 (404) 482-5929
+(inspect (% 27 2))
+(inspect (+ 271 (/ 300 3) 52 59))
+(inspect (* 7 11 (- 99 22)))
